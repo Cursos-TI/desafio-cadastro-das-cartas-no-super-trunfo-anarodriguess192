b46f70c9564da7430e6a112013f1f17b7f86cbe6
anarodriguess192 <anarodriguess2008@hotmail.com>
2025-06-15 19:30:26 -0300

    Update CartasSuperTrunfo.c
    Print

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,39 @@
 #includ <stdio.h>
-intmain(){
-    int poulação=  12325
-    int pontos turisticos= 50 pontos;
-  char  estado = Alagoas;
-
-  char codigo = a01;
- char area area = 1521.11km
- char   área =152111;
- char  pib= 699;
-   char cidade= Maragogi;
-
-   printf("poulação: %d 12325");
-    printf("pontos turisticos: %d 50")
-
-
-   return 0;
- 
-       }
+'int main()'{  // Variaveis para carta1
+    
+    int populacao = 20;
+     int pontos_ turisticos = 50;
+   char estado[50] = "Alagoas";
+    char codigo[10] = "A01";
+  char cidade[50] = "Maragogi";
+  float area = 1521.11 km;  
+  float pib = 699.0;
+    
+    // variaveis para carta2
+    
+    int populacao = 30;
+    int pontos_turisticos= 40;
+    char estado[30] = "Riodejaneiro";
+    char codigo[20] ="B01
+    char cidade[30] = "Botafogo";
+    float area = 20310.12 km;
+    float pib = 800,0;
+    
+    
+    
+  
+// Exibindo os dados da carta
+    printf("--- Dados da Carta ---\n");
+    printf("Estado: %s\n", estado);
+  printf("Codigo da Carta: %s\n", codigo);
+    printf("Cidade: %s\n", cidade);
+    printf("Populacao: %d habitantes\n", populacao);
+    printf("Area: %.2f km^2\n", area); 
+    printf("PIB: %.2f\n", pib); 
+     printf("Pontos Turisticos: %d\n", pontos_turisticos);
+    
+    return0;
+  }
 
 
 
